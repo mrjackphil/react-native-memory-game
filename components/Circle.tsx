04764470de8a4b24937e3166968c6f21c514d3ca
DegtyarEO <geimerus@gmail.com>
2019-02-21 11:10:02 +0300

    Refactor circle component

--- components/Circle.tsx
+++ components/Circle.tsx
@@ -1,31 +1,70 @@
 import React from 'react';
-import { StyleSheet, Image, TouchableHighlight } from 'react-native';
-import { circles } from '../models/circles';
-import { circlesFull } from '../models/circlesFull';
+import { StyleSheet, TouchableHighlight, Animated } from 'react-native';
 
 interface Props {
   color: string;
   full: boolean;
-  onLoaded: () => void;
-  onClick: (s: string) => void;
+  onClick?: Function;
 }
 
-export default class Circle extends React.Component<Props> {
+interface State {
+  fade: Animated.Value;
+}
+
+export default class Circle extends React.Component<Props, State> {
+  rgb: { [k: string]: string } = {
+    red: '255, 85, 85',
+    orange: '255, 153, 85',
+    green: '85, 255, 153',
+    purple: '255, 85, 221',
+    yellow: '255, 230, 128',
+    blue: '128, 229, 255'
+  };
   constructor(props: Props) {
     super(props);
+
+    this.state = {
+      fade: new Animated.Value(0)
+    };
+  }
+
+  onHandleClick = () => {
+    this.props.onClick ? this.props.onClick() : {};
+  };
+
+  get fadeStart() {
+    Animated.sequence([
+      Animated.timing(this.state.fade, {
+        toValue: 1
+      }),
+      Animated.timing(this.state.fade, {
+        toValue: 0,
+        duration: 500
+      })
+    ]).start();
+    return true;
   }
 
   render() {
+    const backgroundColor = this.state.fade.interpolate({
+      inputRange: [0, 1],
+      outputRange: [
+        `rgba(${this.rgb[this.props.color]}, 0)`,
+        `rgba(${this.rgb[this.props.color]}, 1)`
+      ]
+    });
+
     return (
-      <TouchableHighlight onPress={() => this.props.onClick(this.props.color)}>
-        <Image
-          style={styles.bubble}
-          source={
-            this.props.full
-              ? circlesFull[this.props.color]
-              : circles[this.props.color]
-          }
-          onLoadEnd={this.props.onLoaded}
+      <TouchableHighlight onPress={this.onHandleClick}>
+        <Animated.View
+          style={[
+            styles.circle,
+            { borderColor: `rgb(${this.rgb[this.props.color]})` },
+            this.props.full &&
+              this.fadeStart && {
+                backgroundColor: backgroundColor
+              }
+          ]}
         />
       </TouchableHighlight>
     );
@@ -33,8 +72,11 @@ export default class Circle extends React.Component<Props> {
 }
 
 const styles = StyleSheet.create({
-  bubble: {
+  circle: {
     width: 40,
-    height: 40
+    height: 40,
+    borderWidth: 4,
+    borderStyle: 'solid',
+    borderRadius: 50
   }
 });
