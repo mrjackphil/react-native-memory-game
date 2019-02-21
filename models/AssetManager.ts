ee007708e66c50a2265aaadb135538035b2faad0
DegtyarEO <geimerus@gmail.com>
2019-02-21 11:08:52 +0300

    Add asset manager

---
+++ models/AssetManager.ts
@@ -0,0 +1,7 @@
+import { circles } from './AssetManager/circles';
+import { circlesFull } from './AssetManager/circlesFull';
+
+export default class AssetManager {
+	public empty = circles;
+	public full = circlesFull;
+}
